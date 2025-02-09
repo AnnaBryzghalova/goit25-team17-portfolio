53d144236edb50f4687a8d17ce2de3dd1b968776
AnnaBryzghalova <vojnoannette@gmail.com>
2025-02-09 16:33:38 +0200

    Hide arrows, when no reviews loaded. Added more specification to CSS selectors.

--- src/js/07-reviews.js
+++ src/js/07-reviews.js
@@ -1,13 +1,23 @@
-const reviewsList = document.querySelector('#reviews .swiper-wrapper');
-
 import { fetchResponses, sendCooperationRequest } from './goit-api';
 import { renderReviewItems } from './render-functions';
 import { initSwiper } from './swiper-api';
 
-try {
-  const response = await fetchResponses();
-  reviewsList.innerHTML = renderReviewItems(response);
+const reviewsList = document.querySelector('#reviews .swiper-wrapper');
 
+async function tryRenderResponses() {
+  try {
+    const response = await fetchResponses();
+    reviewsList.innerHTML = renderReviewItems(response);
+    return true;
+  } catch (error) {
+    console.error('Error fetching reviews:', error);
+    reviewsList.innerHTML = '<li class="swiper-slide">Not found</li>';
+    return false;
+  }
+}
+
+const rendered = await tryRenderResponses();
+if (rendered) {
   const swiperParams = {
     spaceBetween: 16,
     breakpoints: {
@@ -22,7 +32,8 @@ try {
     useNavigation: true,
     additionalParams: swiperParams,
   });
-} catch (error) {
-  console.error('Error fetching reviews:', error);
-  reviewsList.innerHTML = '<li class="swiper-slide">Not found</li>';
+} else {
+  document
+    .querySelector('#reviews .side-arrows')
+    .classList.add('visually-hidden');
 }
