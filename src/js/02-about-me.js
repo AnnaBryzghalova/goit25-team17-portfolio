58314dc77f9376b56b1b0f248e9dfca080ee4718
Vitalii-Karashchuk <83291711+Vitalii-Karashchuk@users.noreply.github.com>
2025-02-11 16:24:27 +0200

    Swiper works. Accordion wip

--- src/js/02-about-me.js
+++ src/js/02-about-me.js
@@ -1,39 +1,9 @@
-import Accordion from "accordion-js";
-import "accordion-js/dist/accordion.min.css";
+// import Accordion from 'accordion-js';
 
-// Accordion
-const acEl = document.querySelector('.accord-title');
-console.log("acEl:", acEl)
+// console.log(document.querySelector('#about-me .accordion-container'));
 
-
-const handleAc = (ev) => {
-  new Accordion("#about-me .accordion", {
-  duration: 400,
-  showMultiple: true,
- 
-  });
-  // ev.target.closest('accord-title');
-};
-
-
-
-acEl.addEventListener('click', handleAc);
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   new Accordion(".accordion-container", {
-//     duration: 400, // Швидкість анімації (мс)
-//     showMultiple: false, // Закривати попередні блоки при відкритті нового
-//     collapse: true, // Дозволити закривати всі блоки
-//   });
+// new Accordion('#about-me .accordion-container', {
 // });
-
-
-
-
-
-
-
 
 
 
@@ -41,46 +11,8 @@ acEl.addEventListener('click', handleAc);
 
 import { initSwiper } from './swiper-api';
 
-
-
-const btnSwiperEl = document.querySelector('.btn-slide');
-// const swiper = document.querySelector('.swiper').swiper;
-
-// const handleClickSwiper = () => {
-//     const swiper = new Swiper('.swiper', {
- 
-//       slidesPerView: "auto",
-//       spaceBetween: 0,
-//       loop: true,
-//       enabled: true,
-//       navigation: {
-//         nextEl: ".btn-slide",
-//       },
-//       breakpoints: {
-//         320: { slidesPerView: 2 },
-//         768: { slidesPerView: 3 },
-//         1440: { slidesPerView: 6 },
-//       },
-        
-//     });
-  
-//         initSwiper({
-//       sectionId: 'reviews',
-//       useNavigation: true,
-      
-//     });
-     
-// };
-
-
-// btnSwiperEl.addEventListener('click', handleClickSwiper);
-
-
-const swiper = new Swiper('.swiper', {
-  slidesPerView: "auto",
-  spaceBetween: 0,
+const swiperOptions = {
   loop: true,
-  enabled: true,
   navigation: {
     nextEl: ".btn-slide",
   },
@@ -88,39 +20,11 @@ const swiper = new Swiper('.swiper', {
     320: { slidesPerView: 2 },
     768: { slidesPerView: 3 },
     1440: { slidesPerView: 6 },
-  },
-});
+  }
+};
 
 initSwiper({
   sectionId: 'about-me',
   useNavigation: true,
+  additionalParams: swiperOptions,
 });
-
-
-btnSwiperEl.addEventListener('click', () => swiper.slideNext());
-
-
-
-// const reviewsList = document.querySelector('#about-me .swiper-wrapper');
-// reviewsEntryPoint();
-
-// async function reviewsEntryPoint() {
-//   const rendered = await tryRenderReviews();
-//   if (rendered) {
-//     const swiperParams = {
-//       spaceBetween: 0,
-//       breakpoints: {
-//         320: { slidesPerView: 1 },
-//         768: { slidesPerView: 2 },
-//         1440: { slidesPerView: 4 },
-//       },
-//     };
-
-//     initSwiper({
-//       sectionId: 'about-me',
-//       useNavigation: true,
-//       additionalParams: swiperParams,
-//     });
-//         reviewsList.style = 'justify-content: center';
-//   }
-// };      
